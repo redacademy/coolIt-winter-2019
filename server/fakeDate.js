211d642685c0f4f7e31f76f641c49011dbc6c682
Steve Choi <stevechoi93@gmail.com>
2019-03-12 11:42:33 -0700

    added fake user data according to schemda

---
+++ server/fakeDate.js
@@ -0,0 +1,29 @@
+export const user1 = {
+  id: 111,
+  name: "Victoria",
+  email: "Victoria@gmail.com",
+  password: "asdfasdf",
+  schoolInfo: {
+    division: "1"
+  },
+  programCode: 1231,
+  activityDate: [1, 3, 2, 5],
+  point: 5120,
+  isActive: true,
+  quizScore: 1231
+};
+
+export const user2 = {
+  id: 222,
+  name: "VictoTria",
+  email: "VictoTria@gmail.com",
+  password: "asdfasdf",
+  schoolInfo: {
+    division: "1"
+  },
+  programCode: 1211231,
+  activityDate: [1, 3, 2, 5],
+  point: 515420,
+  isActive: true,
+  quizScore: 166
+};
