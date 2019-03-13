235a4f2a2ef0a290fed586a18068cc49b38e5d77
Sophie Virtue <sophie.virtue@gmail.com>
2019-03-13 14:50:47 -0700

    Create and style AccountCreated page

--- client/App.js
+++ client/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from "react";
 import { View } from "react-native";
-import Landing from "./js/screens/Landing";
+import AccountCreated from "./js/screens/AccountCreated";
 
 export default class App extends Component {
   render() {
     return (
       <View>
-        <Landing />
+        <AccountCreated />
       </View>
     );
     }}
--- client/js/screens/AccountCreated/styles.js
+++ client/js/screens/AccountCreated/styles.js
@@ -2,7 +2,45 @@ import { StyleSheet } from "react-native";
 import { colors, fonts } from "../../config/styles";
 
 const styles = StyleSheet.create({
- 
+  container: {
+    backgroundColor: colors.white,
+    height: "100%",
+    width: "100%"
+  },
+  bottom: {
+    width: "100%",
+    height: "75%"
+  },
+  backgroundBottom: {
+    flex: 1,
+    justifyContent: "flex-end"
+  },
+  top: {
+    width: "100%",
+    height: "75%"
+  },
+  backgroundTop: {
+    flex: 1,
+    justifyContent: "flex-start"
+  },
+  check: {
+    width: 100,
+    height: 100
+  },
+  flexContent: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "space-around"
+  },
+  welcome: {
+    fontSize: 24,
+    fontFamily: fonts.light, 
+    fontWeight: 'normal',
+  },
+  created: {
+    fontFamily: fonts.light,
+    fontSize: 20
+  }
 });
 
 export default styles;
