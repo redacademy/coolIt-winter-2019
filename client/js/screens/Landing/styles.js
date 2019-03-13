c0838fb96a502b16f38c7307d8b1737e0e21a140
Sophie Virtue <sophie.virtue@gmail.com>
2019-03-13 15:59:23 -0700

    Resolve issues for this feature

--- client/js/screens/Landing/styles.js
+++ client/js/screens/Landing/styles.js
@@ -2,11 +2,21 @@ import { StyleSheet } from "react-native";
 import { colors, fonts } from "../../config/styles";
 
 const styles = StyleSheet.create({
-  container: {},
+  // container: {
+  //   alignItems: "center",
+  //   justifyContent: "center",
+  //   // paddingTop: 120
+  // },
   center: {
     alignItems: "center",
-    paddingRight: 30,
-    paddingLeft: 30
+    flex: 1.4,
+    justifyContent: "space-around", 
+    paddingTop: 120
+  },
+  flexText: {
+    alignItems: "center",
+    flex: 0.7,
+    justifyContent: "space-between"
   },
   background: {
     width: "100%",
@@ -14,64 +24,57 @@ const styles = StyleSheet.create({
   },
   text: {
     fontFamily: fonts.light,
-    textAlign: 'center', 
+    textAlign: "center",
     fontSize: 16,
-    paddingBottom: 30
+    width: 300
   },
   join: {
     fontFamily: fonts.light,
-    textAlign: 'center', 
-    fontSize: 18,
-    paddingBottom: 30
+    textAlign: "center",
+    fontSize: 18
   },
   checkbox: {
     fontFamily: fonts.light,
-    textAlign: 'center', 
-    fontSize: 14, 
+    textAlign: "center",
+    fontSize: 14,
     paddingLeft: 15
   },
   logo: {
-    width: "63%",
-    height: "5.6%",
-    marginTop: 120,
-    marginBottom: 25
+    width: 212,
+    height: 35
   },
   valley: {
     width: "100%",
-    height: "62%"
+    height: "46%"
   },
   bottom: {
-   flex: 1, 
-   justifyContent: 'flex-end'
+    flex: 1,
+    justifyContent: "flex-end"
   },
   joinUs: {
-    margin: 20,
-    marginBottom: 30,
-    marginTop: 40,
-    width: '50%',
-    alignSelf: 'center',
+    width: "40%",
+    alignSelf: "center",
     backgroundColor: colors.blue,
     borderRadius: 13
   },
   logIn: {
-    margin: 20,
-    width: '50%',
-    alignSelf: 'center',
-    backgroundColor: colors.green, 
+    width: "40%",
+    alignSelf: "center",
+    backgroundColor: colors.green,
     borderRadius: 13
-  }, 
+  },
   buttonText: {
-    alignSelf: 'center',
-    fontFamily: 'Arial',
+    alignSelf: "center",
+    fontFamily: "Arial",
     fontSize: 16,
     color: colors.white,
     paddingTop: 10,
     paddingBottom: 10
-  }, 
+  },
   flex: {
     flex: 0,
-    flexDirection: 'row',
-    justifyContent: 'center'
+    flexDirection: "row",
+    justifyContent: "center"
   }
 });
 
