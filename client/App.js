f29d6414ae460b3f7a2fc82bfc5659bf1c556d1f
Sophie Virtue <sophie.virtue@gmail.com>
2019-03-14 15:40:13 -0700

    Create and style Take Quiz and Begin Challenge screens

--- client/App.js
+++ client/App.js
@@ -1,12 +1,12 @@
 import React, {Component} from "react";
-import Score from './js/screens/Score'
+import BeginChallenge from './js/screens/BeginChallenge'
 import {StatusBar} from "react-native";
 
 export default class App extends Component {
   render() {
     StatusBar.setBarStyle("light-content", true);
     return (
-      <Score />
+      <BeginChallenge />
     );
   }
 }
--- client/js/screens/BeginChallenge/styles.js
+++ client/js/screens/BeginChallenge/styles.js
@@ -2,7 +2,61 @@ import { StyleSheet } from "react-native";
 import { colors, fonts } from "../../config/styles";
 
 const styles = StyleSheet.create({
- 
+  center: {
+    alignItems: "center",
+    flex: 1.25,
+    justifyContent: "space-around",
+    paddingTop: 120
+  },
+  flexText: {
+    alignItems: "center",
+    flex: 0.9,
+    justifyContent: "space-between",
+    paddingBottom: 60,
+    paddingTop: 30
+  },
+  background: {
+    width: "100%",
+    height: "100%"
+  },
+  text: {
+    fontFamily: fonts.light,
+    textAlign: "center",
+    fontSize: 14,
+    width: 300
+  },
+  logo: {
+    width: 180,
+    height: 30,
+    paddingBottom: 0
+  },
+  challenge: {
+    fontFamily: fonts.light,
+    fontWeight: 'bold',
+    fontSize: 20
+  },
+  valley: {
+    width: "100%",
+    height: "47%"
+  },
+  bottom: {
+    flex: 1,
+    justifyContent: "flex-end"
+  },
+  button: {
+    width: "60%",
+    alignSelf: "center",
+    backgroundColor: colors.green,
+    borderRadius: 13
+  },
+  buttonText: {
+    alignSelf: "center",
+    fontFamily: fonts.light,
+    fontSize: 18,
+    color: colors.white,
+    paddingTop: 8,
+    paddingBottom: 8
+  }
 });
 
 export default styles;
