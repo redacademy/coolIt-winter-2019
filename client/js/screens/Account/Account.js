af1ca074291a91eb17d2396a3c7f4dd456dc42c1
Timmy <timgabrielnguyen@gmail.com>
2019-03-18 15:24:58 -0700

    Added Content and Styled Account Screen

--- client/js/screens/Account/Account.js
+++ client/js/screens/Account/Account.js
@@ -1,11 +1,53 @@
 import React from "react";
-import { View, Text } from "react-native";
+import {View, Text, ScrollView, Image, TouchableOpacity} from "react-native";
+import styles from "./styles";
 
 const Accounts = props => {
   return (
-    <View>
-      <Text> Hi im accounts</Text>
-    </View>
+    <ScrollView contentContainerStyle={styles.contentContainer}>
+      <View style={styles.container}>
+        <View style={styles.mainContent}>
+          <View style={styles.itemSeparator} />
+          <Text style={styles.description}>Account</Text>
+          <View style={styles.itemBottomSeparator} />
+        </View>
+
+        <View style={styles.menu}>
+          <View style={styles.buttonSeparator} />
+          <TouchableOpacity onPress={() => {}} style={styles.logIn}>
+            <Text style={styles.buttonText}>Change Password</Text>
+          </TouchableOpacity>
+          <View style={styles.buttonSeparator} />
+
+          <View style={styles.buttonSeparator} />
+          <TouchableOpacity onPress={() => {}} style={styles.logIn}>
+            <Text style={styles.buttonText}>
+              Disconnect My Account From The Cool It Program
+            </Text>
+          </TouchableOpacity>
+          <View style={styles.buttonSeparator} />
+
+          <View style={styles.buttonSeparator} />
+          <TouchableOpacity onPress={() => {}} style={styles.logIn}>
+            <Text style={styles.buttonText}>Deactivate Account</Text>
+          </TouchableOpacity>
+          <View style={styles.buttonSeparator} />
+
+          <View style={styles.buttonSeparator} />
+          <TouchableOpacity onPress={() => {}} style={styles.logIn}>
+            <Text style={styles.buttonText}>Log out </Text>
+          </TouchableOpacity>
+          <View style={styles.buttonSeparator} />
+        </View>
+
+        <View style={styles.bottom}>
+          <Image
+            source={require("../../assets/images/valley.png")}
+            style={styles.valley}
+          />
+        </View>
+      </View>
+    </ScrollView>
   );
 };
 
