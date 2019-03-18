bef5cae96440846e0ed598e36e2acfa47db6694b
Sophie Virtue <sophie.virtue@gmail.com>
2019-03-18 16:09:21 -0700

    Work on final form for progrm code page

--- client/js/screens/ProgramCode/ProgramCode.js
+++ client/js/screens/ProgramCode/ProgramCode.js
@@ -1,12 +1,44 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, Image, TouchableOpacity } from "react-native";
+import { Form, Field } from "react-final-form";
 import styles from "./styles";
 // import PropTypes from "prop-types";
 
 const ProgramCode = () => {
   return (
-    <View>
-      <Text>ProgramCode Screen!</Text>
+    <View style={styles.container}>
+      <View style={styles.backgroundTop}>
+        <Image
+          source={require("../../assets/images/background2-top.png")}
+          style={styles.top}
+        />
+      </View>
+      <View style={styles.flexContent}>
+        <Text style={styles.text}>If you are already</Text>
+        <Text style={styles.text}>part of the Cool It! Program,</Text>
+        <Text style={styles.text}>Please provide:</Text>
+        <Form
+          onSubmit={() => {}}
+          render={() => (
+            <form>
+              <Field name="school" placeholder="School" />
+              <Field name="teacher" placeholder="Teacher" />
+              <TouchableOpacity onPress={() => {}} style={styles.button}>
+                <Text style={styles.buttonText}>Continue</Text>
+              </TouchableOpacity>
+              <TouchableOpacity onPress={() => {}}>
+                <Text style={styles.skipText}>Skip</Text>
+              </TouchableOpacity>
+            </form>
+          )}
+        />
+      </View>
+      <View style={styles.backgroundBottom}>
+        <Image
+          source={require("../../assets/images/background2-bottom.png")}
+          style={styles.bottom}
+        />
+      </View>
     </View>
   );
 };
--- client/js/screens/ProgramCode/styles.js
+++ client/js/screens/ProgramCode/styles.js
@@ -1,8 +1,64 @@
 import { StyleSheet } from "react-native";
 import { colors, fonts } from "../../config/styles";
-
 const styles = StyleSheet.create({
- 
+  container: {
+    backgroundColor: colors.white,
+    height: "100%",
+    width: "100%"
+  },
+  bottom: {
+    width: "100%",
+    height: "75%"
+  },
+  backgroundBottom: {
+    flex: 1,
+    justifyContent: "flex-end"
+  },
+  top: {
+    width: "100%",
+    height: "75%"
+  },
+  backgroundTop: {
+    flex: 1,
+    justifyContent: "flex-start"
+  },
+  check: {
+    width: 100,
+    height: 100
+  },
+  flexContent: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "space-around",
+    paddingLeft: 30,
+    paddingRight: 30
+  },
+  text: {
+    fontSize: 18,
+    fontFamily: fonts.light,
+    alignSelf: 'flex-start'
+  },
+  button: {
+    width: "50%",
+    alignSelf: "center",
+    backgroundColor: colors.green,
+    borderRadius: 13
+  },
+  buttonText: {
+    alignSelf: "center",
+    fontFamily: fonts.light,
+    fontSize: 16,
+    color: colors.white,
+    paddingTop: 8,
+    paddingBottom: 8
+  },
+  skipText: {
+    alignSelf: "center",
+    fontFamily: fonts.light,
+    fontSize: 14,
+    color: colors.black,
+    paddingTop: 8,
+    paddingBottom: 8
+  }
 });
-
 export default styles;
