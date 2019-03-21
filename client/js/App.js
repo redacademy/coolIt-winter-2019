3b82f2b5a032b91a94b391d80c3d92f4b4f040cb
Timmy <timgabrielnguyen@gmail.com>
2019-03-21 16:02:15 -0700

    Validation for Program Code Page

--- client/js/App.js
+++ client/js/App.js
@@ -1,15 +1,17 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import client from "../js//config/api";
-import { ApolloProvider } from "react-apollo";
+import {ApolloProvider} from "react-apollo";
 import RootStackNavigator from "../js/navigation/RootStackNavigator";
-import { StatusBar } from "react-native";
+import {StatusBar} from "react-native";
+import ProgramCode from "./screens/ProgramCode";
 
 export default class App extends Component {
   render() {
     StatusBar.setBarStyle("dark-content", true);
     return (
       <ApolloProvider client={client}>
-        <RootStackNavigator />
+        <ProgramCode />
+        {/* <RootStackNavigator /> */}
       </ApolloProvider>
     );
   }
