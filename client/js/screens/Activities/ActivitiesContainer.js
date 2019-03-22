c92a616d0f37e956968beee3d70b8f03883e7c81
Steve Choi <stevechoi93@gmail.com>
2019-03-21 17:39:21 -0700

    landing page navigation implemented

--- client/js/screens/Activities/ActivitiesContainer.js
+++ client/js/screens/Activities/ActivitiesContainer.js
@@ -48,13 +48,13 @@ class ActivitiesContainer extends Component {
     };
   }
   componentDidMount = async () => {
+    const userId = await AsyncStorage.getItem("id");
     const today = new Date();
     today.setHours(0);
     today.setMinutes(0);
     today.setSeconds(0);
     today.setMilliseconds(0);
     console.log(today);
-    const userId = await AsyncStorage.getItem("id");
     this.setState({ date: today, userId });
   };
   dateChangeHandler = getNextDay => {
--- client/js/screens/Landing/LandingContainer.js
+++ client/js/screens/Landing/LandingContainer.js
@@ -2,9 +2,14 @@ import React, { Component } from "react";
 import Landing from "./Landing";
 
 export default class LandingContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { checked: false };
+  }
+  checkBox = () => {
+    this.setState({ checked: !this.state.checked });
+  };
   render() {
-    return (
-      <Landing />
-    );
+    return <Landing checked={this.state.checked} checkBox={this.checkBox} />;
   }
 }
