f5ebe679b8987fd2037899d5d95c63527645ba03
Vikkkkk <vicguo.99@gmail.com>
2019-03-26 09:37:27 -0700

    putting the icons on earth

--- client/js/screens/Planet/styles.js
+++ client/js/screens/Planet/styles.js
@@ -1,11 +1,6 @@
-import {StyleSheet, Dimensions} from "react-native";
+import { StyleSheet, Dimensions } from "react-native";
 
 const styles = StyleSheet.create({
-  image: {
-    resizeMode: "contain",
-    padding: 20,
-    marginTop: 20
-  },
   imageContainer: {
     padding: 20
   },
@@ -15,6 +10,22 @@ const styles = StyleSheet.create({
   },
   iconImage: {
     padding: 12
+  },
+  baby: { paddingLeft: "45%", marginTop: "25%", resizeMode: "contain" },
+  flower: {
+    flexDirection: "row",
+    width: "100%",
+    justifyContent: "flex-start",
+    resizeMode: "contain",
+    marginLeft: "20%"
+  },
+  mountain: { paddingLeft: "60%", marginTop: "5%", resizeMode: "contain" },
+  tree: {
+    flexDirection: "row",
+    width: "70%",
+    justifyContent: "flex-end",
+    resizeMode: "contain",
+    marginTop: "20%"
   }
 });
 
