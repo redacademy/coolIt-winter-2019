25115a070ad6adc0845241db416bec41f56e9412
Timmy <timgabrielnguyen@gmail.com>
2019-03-27 19:46:37 -0700

    Cleaned up Config Folder

--- client/js/config/styles.js
+++ client/js/config/styles.js
@@ -1,11 +1,10 @@
 export const colors = {
   white: "#fff",
   black: "#000",
-  blue: '#3A61AE',
-  green: '#3B5327',
+  blue: "#3A61AE",
+  green: "#3B5327",
   lightGreen: "#96a687",
   disabled: "#dee0db",
-  red: 'red',
   darkGreen: "#4F7627",
   gray: "#D8D8D8",
   red: "#E74C3C"
